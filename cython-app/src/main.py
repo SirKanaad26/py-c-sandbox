ab8284aeac0d8b9cc661b2e94441087263699ccd
Nanda <kanakapuramsrinivasa@ucsd.edu>
2025-05-15 16:18:58 -0700

    Added Sandbox Framework

--- cython-app/src/main.py
+++ cython-app/src/main.py
@@ -1,7 +1,7 @@
 # main.py
-from capitalize_wrapper import py_capitalize
+from capitalize_wrapper import cy_capitalize
 
 if __name__ == "__main__":
     s = "hello, world!"
     print("Original:", s)
-    print("Capitalized:", py_capitalize(s))
+    print("Capitalized:", cy_capitalize(s))
