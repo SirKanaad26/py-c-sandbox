15cf377c3c1a5c17e960746eafb197bffb7aa342
manojgayala <magayala@ucsd.edu>
2025-05-24 21:39:53 -0500

    Wasm module and js code for snappy

--- cython-app/src/setup.py
+++ cython-app/src/setup.py
@@ -4,7 +4,7 @@ from Cython.Build import cythonize
 ext = Extension(
     name="capitalize_wrapper",
     # sources=["capitalize_wrapper.pyx", "capitalize.c"],  # Do NOT include .h here
-    sources=["wasm_capitalize_wrapper.pyx"],  # Do NOT include .h here
+    sources=["wasm_capitalize_wrapper.pyx", "capitalize.c"],  # Do NOT include .h here
     include_dirs=["."],  # The .h file should be in this directory
     language="c",
 )
