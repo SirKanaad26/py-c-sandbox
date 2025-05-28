366cca2337749f2ceab98d00d5f8ce33a404f08f
Nanda <kanakapuramsrinivasa@ucsd.edu>
2025-05-28 04:12:29 -0700

    Added snappy_unsandboxed

--- snappy_unsandboxed/py_c_connector.pyx
+++
@@ -1,6 +0,0 @@
-cdef extern from "snappy/snappy_wrapper.h":
-    size_t cython_MaxCompressedLength(size_t source_bytes)
-
-cdef class CythonClass:
-    def cython_max_compressed_length(self, source_bytes):
-        return cython_MaxCompressedLength(source_bytes)
