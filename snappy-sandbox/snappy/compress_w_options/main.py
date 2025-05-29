8da7963bead5fa48011685230a86921f7669fcf4
manojgayala <magayala@ucsd.edu>
2025-05-29 04:36:28 -0700

    Snappy compress with options

--- snappy-sandbox/snappy/compress_w_options/main.py
+++ snappy-sandbox/snappy/compress_w_options/main.py
@@ -4,7 +4,7 @@ from snappywasm.core import SnappyWasm
 def main():
     snappy = SnappyWasm()
     data = b"hello world " * 100
-    compressed = snappy.compress(data,1)
+    compressed = snappy.compress(data, 2)
     print("Compressed Length:", len(compressed))
     print("Original Length:", len(data))
 
