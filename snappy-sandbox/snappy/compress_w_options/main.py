a14f622c11897da392ca600baffd523e524f8a41
SirKanaad26 <kaninamerica@gmail.com>
2025-06-07 23:06:45 -0700

    Compress with options - core updated.

--- snappy-sandbox/snappy/compress_w_options/main.py
+++ snappy-sandbox/snappy/compress_w_options/main.py
@@ -1,8 +1,11 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from snappywasm.core import SnappyWasm
 
 def main():
     snappy = SnappyWasm()
-    data = b"hello worldsajisijqbwnedjbideiwidjqbwdjhqwbdjbkdjjkdajksdjka" * 100
+    data = b"hello worldsajisijqbwnedjbideiwidjqbwdjhqwbdjbkdjjkdajksdjka" * 50
     compressed = snappy.compress(data, 2)
     print("Compressed Length:", len(compressed))
     print("Original Length:", len(data))
