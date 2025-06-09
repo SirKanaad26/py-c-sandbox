987e8269952b780afe4c5e93e3757085979810f4
manojgayala <magayala@ucsd.edu>
2025-06-09 00:18:43 -0700

    App

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Sandboxing in Python
 This repo contains the source code for all the experiments we did as part of CSE 227 project.
+
+# Instructions to run code
+python setup.py build_ext --inplace
+python app/main.py
+
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from cython_snappy import CythonClass
+from fastapi import FastAPI, UploadFile, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from wasm_runtime.wasm_executor import SnappyWasm
 
-if __name__ == "__main__":
-    snappy = CythonClass()
-    
-    source_length = 1000  # Size in bytes of data you want to compress
-    compressed_bound = snappy.cython_MaxCompressedLength(source_length)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-    print(f"Maximum compressed size for {source_length} bytes: {compressed_bound}")
+snappy = SnappyWasm("snappy_bindings/snappy.wasm")
+
+@app.get("/", response_class=HTMLResponse)
+async def homepage():
+    with open("static/index.html", "r") as f:
+        return f.read()
+
+@app.post("/compress/")
+async def compress_text(data: str = Form(...)):
+    input_bytes = data.encode("utf-8")
+    compressed = snappy.compress(input_bytes)
+    return {"original_size": len(input_bytes), "compressed_size": len(compressed)}
