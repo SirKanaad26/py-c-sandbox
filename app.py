98760569a2e3190b95a29c141044b34e826c9cc2
manojgayala <magayala@ucsd.edu>
2025-06-09 23:07:37 -0700

    Added the core logic of main application

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, UploadFile, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
-from wasm_runtime.wasm_executor import SnappyWasm
+from snappy_unsandboxed import snappy_wrapper
+from snappy_sandbox.snappy.snappywasm import snappy_sandbox
+import base64
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-snappy = SnappyWasm("snappy_bindings/snappy.wasm")
 
 @app.get("/", response_class=HTMLResponse)
 async def homepage():
@@ -14,7 +14,18 @@ async def homepage():
         return f.read()
 
 @app.post("/compress/")
-async def compress_text(data: str = Form(...)):
+async def compress_text(data: str = Form(...), mode: str = Form(...)):
     input_bytes = data.encode("utf-8")
-    compressed = snappy.compress(input_bytes)
-    return {"original_size": len(input_bytes), "compressed_size": len(compressed)}
+    if mode == "sandboxed":
+        snappy_sandboxed = snappy_sandbox.SnappyWasm()
+        compressed = snappy_sandboxed.compress(input_bytes)
+    else:
+        compressed = snappy_wrapper.compress_data(input_bytes)
+
+    encoded = base64.b64encode(compressed).decode("utf-8")
+    return JSONResponse({
+        "original_size": len(input_bytes),
+        "compressed_size": len(compressed),
+        "compressed_base64": encoded
+    })
+
---
+++ snappy_sandbox/snappy/snappywasm/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import os
+
+# Full dotted module name
+ext_modules = [
+    Extension(
+        name="snappy_sandbox",                # key: must match Python package name
+        sources=["snappy_sandbox.pyx"],       # path relative to project root
+        extra_link_args=[
+            "-Wl,-undefined,dynamic_lookup" if os.uname().sysname == "Darwin" else ""
+        ],
+    )
+]
+
+setup(
+    name="smappy_sandbox",
+    ext_modules=cythonize(
+        ext_modules,
+        compiler_directives={'language_level': 3},
+        annotate=True
+    ),
+    # packages=["snappywasm"],
+    zip_safe=False,
+)
+
+
