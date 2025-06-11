121c40243532b9b4afacf40750cad13b1b3ecfc9
SirKanaad26 <kaninamerica@gmail.com>
2025-06-10 23:41:31 -0700

    Enhanced UI.

--- snappy_sandbox/snappy/compressFromIOVec/main.py
+++ snappy_sandbox/snappy/compressFromIOVec/main.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from snappywasm.core import SnappyWasm
+from snappywasm.snappy_sandbox import SnappyWasm
 
 def main():
     snappy = SnappyWasm()
